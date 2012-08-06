47655d52f9417b0e6b851662ade9c493fdcecf5a
Eric Rushing <rushinge@gmail.com>
2012-08-05 18:30:26 -0700

    Implement AxesView drawing routine

--- Calculator/AxesView.h
+++ Calculator/AxesView.h
@@ -8,6 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
+
+@protocol AxesViewDataSource
+
+- (double)getYForX:(double)x;
+
+@end
+
+
 @interface AxesView : UIView
+
+@property (readonly) CGPoint origin;
+@property (nonatomic) CGFloat scale; // points per unit
+@property (nonatomic, weak) id <AxesViewDataSource> dataSource;
 
 @end
--- Calculator/AxesView.m
+++ Calculator/AxesView.m
@@ -8,28 +8,50 @@
 #import "AxesDrawer.h"
 
 
+#define DEFAULT_SCALE 20;
+
+
 @implementation AxesView
 
-- (id)initWithFrame:(CGRect)frame
-{
-    self = [super initWithFrame:frame];
-  
-    return self;
+@synthesize scale = scale_;
+@synthesize dataSource = dataSource_;
+
+- (CGFloat)scale {
+  if (!scale_) self.scale = DEFAULT_SCALE;
+  return scale_;
+}
+
+- (CGPoint)originFromRect:(CGRect)rect {
+  return CGPointMake(rect.origin.x + rect.size.width / 2,
+                     rect.origin.y + rect.size.height / 2);
 }
 
 - (void)drawRect:(CGRect)rect {
-  CGPoint origin = CGPointMake(rect.origin.x + rect.size.width / 2,
-                               rect.origin.y + rect.size.height / 2);
-  [AxesDrawer drawAxesInRect:rect originAtPoint:origin scale:20];
+  CGPoint origin = [self originFromRect:rect];
+  [AxesDrawer drawAxesInRect:rect originAtPoint:origin scale:self.scale];
+  
+  CGContextRef context = UIGraphicsGetCurrentContext();
+  UIGraphicsPushContext(context);
+  
+  int x = rect.origin.x;
+  double xVal = x / self.scale - rect.origin.x;
+  double yVal = [self.dataSource getYForX:xVal];
+  int y = yVal * (self.scale + rect.origin.y);
+  CGContextMoveToPoint(context, x, y);
+  
+  for (x++; x <= rect.size.width; x++) {
+    xVal = x / self.scale - rect.origin.x;
+    yVal = [self.dataSource getYForX:xVal];
+    y = yVal * (self.scale + rect.origin.y);
+    CGContextAddLineToPoint(context, x, y);
+  }
+  
+  CGColorRef cyan = [UIColor colorWithRed:0 green:0.8 blue:0.8 alpha:1].CGColor;
+  CGContextSetStrokeColorWithColor(context, cyan);
+  CGContextSetLineWidth(context, 1);
+  CGContextStrokePath(context);
+  
+  UIGraphicsPopContext();
 }
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
 
 @end
